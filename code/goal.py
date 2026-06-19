78347c7411635426b2ba7fa17f5e5044e05a0909
gabrielsmg <martins93.gabriel@gmail.com>
2026-06-19 11:31:28 -0300

    Add animated finish flag

--- code/goal.py
+++ code/goal.py
@@ -5,18 +5,40 @@ class Goal:
 
     def __init__(self, x, y):
 
-        self.rect = pygame.Rect(x, y, 64, 128)
-        self.color = (255, 220, 0)
+        self.rect = pygame.Rect(x, y, 96, 128)
+
+        self.frames = [
+            pygame.image.load("../assets/backgrounds/bandeira1.png").convert_alpha(),
+            pygame.image.load("../assets/backgrounds/bandeira2.png").convert_alpha()
+        ]
+
+        self.frames = [
+            pygame.transform.scale(frame, (96, 128))
+            for frame in self.frames
+        ]
+
+        self.frame_index = 0
+        self.animation_timer = 0
+
+    def animate(self):
+
+        self.animation_timer += 1
+
+        if self.animation_timer >= 20:
+            self.animation_timer = 0
+            self.frame_index += 1
+
+            if self.frame_index >= len(self.frames):
+                self.frame_index = 0
 
     def draw(self, screen, camera):
 
-        pygame.draw.rect(
-            screen,
-            self.color,
+        self.animate()
+
+        screen.blit(
+            self.frames[self.frame_index],
             (
                 self.rect.x - camera.offset_x,
-                self.rect.y,
-                self.rect.width,
-                self.rect.height
+                self.rect.y
             )
         )
